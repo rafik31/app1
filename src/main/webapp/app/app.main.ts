166064293436dd2e1bda909f3cb8dcf95a3ecc83
vagrant <vagrant@vagrant.vm>
2017-06-15 14:00:41 +0000

    init

---
+++ src/main/webapp/app/app.main.ts
@@ -0,0 +1,13 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { ProdConfig } from './blocks/config/prod.config';
+import { App1AppModule } from './app.module';
+
+ProdConfig();
+
+if (module['hot']) {
+    module['hot'].accept();
+}
+
+platformBrowserDynamic().bootstrapModule(App1AppModule)
+.then((success) => console.log(`Application started`))
+.catch((err) => console.error(err));
